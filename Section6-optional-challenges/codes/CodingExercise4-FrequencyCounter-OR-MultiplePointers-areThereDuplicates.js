f57994f0ceb6a3e5f2d7ca52c53e3fd2fe9e9076
HazemSabry <hazemsabry2002@gmail.com>
2024-02-02 15:02:12 +0200

    Coding Exercise 4

---
+++ Section6-optional-challenges/codes/CodingExercise4-FrequencyCounter-OR-MultiplePointers-areThereDuplicates.js
@@ -0,0 +1,32 @@
+// Frequency Counter / Multiple Pointers - areThereDuplicates
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+
+// Examples:
+
+// areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates(1, 2, 2) // true
+// areThereDuplicates('a', 'b', 'c', 'a') // true
+// Restrictions:
+
+// Time - O(n)
+
+// Space - O(n)
+
+// Bonus:
+
+// Time - O(n log n)
+
+// Space - O(1)
+
+const areThereDuplicates = (...args) => {
+  const object = {};
+  for (let arg of args) {
+    if (object[arg] !== undefined) return true;
+    object[arg] = 1;
+  }
+  return false;
+};
+
+console.log(areThereDuplicates(1, 2, 3)); // false
+console.log(areThereDuplicates(1, 2, 2)); // true
+console.log(areThereDuplicates("a", "b", "c", "a")); // true
