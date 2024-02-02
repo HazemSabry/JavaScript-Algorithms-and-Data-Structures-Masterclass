b4055e1b89fe36fe5a224c3295344c0d9a8f1473
HazemSabry <hazemsabry2002@gmail.com>
2024-02-02 15:49:18 +0200

    Coding Exercise 6

---
+++ Section6-optional-challenges/codes/CodingExercise6-MultiplePointers-isSubsequence.js
@@ -0,0 +1,31 @@
+// Multiple Pointers - isSubsequence
+// Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+// Examples:
+
+// isSubsequence('hello', 'hello world'); // true
+// isSubsequence('sing', 'sting'); // true
+// isSubsequence('abc', 'abracadabra'); // true
+// isSubsequence('abc', 'acb'); // false (order matters)
+// Your solution MUST have AT LEAST the following complexities:
+
+// Time Complexity - O(N + M)
+
+// Space Complexity - O(1)
+
+const isSubsequence = (string1, string2) => {
+  const string1Array = string1.split("");
+  const string2Array = string2.split("");
+
+  let pointerForString1 = 0;
+  for (let character of string2Array) {
+    if (character === string1Array[pointerForString1]) pointerForString1++;
+  }
+
+  return pointerForString1 === string1Array.length;
+};
+
+console.log(isSubsequence("hello", "hello world")); // true
+console.log(isSubsequence("sing", "sting")); // true
+console.log(isSubsequence("abc", "abracadabra")); // true
+console.log(isSubsequence("abc", "acb")); // false (order matters)
