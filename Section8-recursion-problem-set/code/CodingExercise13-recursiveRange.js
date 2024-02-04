749080fbecd90013b0faf8a573b9426fc24478fd
HazemSabry <hazemsabry2002@gmail.com>
2024-02-04 10:26:35 +0200

    CodingExercise13-recursiveRange

---
+++ Section8-recursion-problem-set/code/CodingExercise13-recursiveRange.js
@@ -0,0 +1,15 @@
+// recursiveRange
+
+// Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55
+
+const recursiveRange = (number) => {
+  if (number === 0) return number;
+  return number + recursiveRange(number - 1);
+};
+
+console.log(recursiveRange(6)); // 21
+console.log(recursiveRange(10)); // 55
