8fab9cbcd5f4d49e683494f83207727d6e3c68cc
HazemSabry <hazemsabry2002@gmail.com>
2024-02-04 11:50:21 +0200

    CodingExercise17-someRecursive

--- Section9-bonus-challenging-recursion-problems/code/CodingExercise17-someRecursive.js
+++ Section9-bonus-challenging-recursion-problems/code/CodingExercise17-someRecursive.js
@@ -11,3 +11,16 @@
 // someRecursive([4,6,8,9], isOdd) // true
 // someRecursive([4,6,8], isOdd) // false
 // someRecursive([4,6,8], val => val > 10); // false
+
+const someRecursive = (array, compareFunction) => {
+  if (array.length === 0) return false;
+  if (compareFunction(array[0])) return true;
+  else return someRecursive(array.slice(1, array.length), compareFunction);
+};
+
+const isOdd = (val) => val % 2 !== 0;
+
+console.log(someRecursive([1, 2, 3, 4], isOdd)); // true
+console.log(someRecursive([4, 6, 8, 9], isOdd)); // true
+console.log(someRecursive([4, 6, 8], isOdd)); // false
+console.log(someRecursive([4, 6, 8], (val) => val > 10)); // false
